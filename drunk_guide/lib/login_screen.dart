5ee31d2fe80a80fe30b20d847b9b02c0c75dd61b
Gyarmathy Bálint <bulint1@gmail.com>
2019-06-23 22:23:04 +0200

    minor improvements

--- drunk_guide/lib/login_screen.dart
+++ drunk_guide/lib/login_screen.dart
@@ -3,23 +3,24 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
-        home: Scaffold(
-            body: Container(
-                child: Column(
-      children: <Widget>[
-        Container(
-          child: Image.asset("assets/temp_logo.png"),
-          padding: EdgeInsets.all(100),
+      home: Scaffold(
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Image.asset("assets/temp_logo.png"),
+                padding: EdgeInsets.all(100),
+              ),
+              MaterialButton(
+                onPressed: () {},
+                child: Text("Login with Google"),
+                color: Colors.blue,
+              )
+            ],
+          ),
         ),
-        Container(
-            child: MaterialButton(
-          onPressed: () {},
-          child: Text("Login with Google"),
-          color: Colors.blue,
-        ))
-      ],
-    ))));
+      ),
+    );
   }
 }
