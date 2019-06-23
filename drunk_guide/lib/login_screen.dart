a639c0f5956985adf5474471bda8448bd461dcec
Gyarmathy Bálint <bulint1@gmail.com>
2019-06-23 22:34:34 +0200

    moved button lower

--- drunk_guide/lib/login_screen.dart
+++ drunk_guide/lib/login_screen.dart
@@ -12,14 +12,17 @@ class LoginScreen extends StatelessWidget {
                 child: Image.asset("assets/temp_logo.png"),
                 padding: EdgeInsets.all(100),
               ),
-              MaterialButton(
-                onPressed: () {},
-                child: Text("Login with Google"),
-                color: Colors.blue,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0)),
-                minWidth: 300,
-                height: 50,
+              Container(
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: Text("Login with Google"),
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  minWidth: 300,
+                  height: 50,
+                ),
+                padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
               )
             ],
           ),
