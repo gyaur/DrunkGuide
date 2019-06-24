6561e6d70d205189c629d09721f525ae8b065cbc
Gyarmathy Bálint <bulint1@gmail.com>
2019-06-24 17:30:51 +0200

    Added settings screen

---
+++ drunk_guide/lib/settings_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _SettingsScreenState();
+  }
+}
+
+Widget settingsItemBuilder(
+    String settingTitle, String settingDescription, Switch switcher) {
+  return Card(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Column(
+          children: <Widget>[Text(settingTitle), Text(settingDescription)],
+        ),
+        switcher,
+      ],
+    ),
+  );
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  List<bool> settings = [false, true, false];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
+        child: Column(
+          children: <Widget>[
+            settingsItemBuilder(
+                "settingTitle",
+                "settingDescription",
+                Switch(
+                  value: settings[0],
+                  onChanged: (tog) {
+                    setState(() {
+                      settings[0] = tog;
+                    });
+                  },
+                )),
+            settingsItemBuilder(
+                "settingTitle",
+                "settingDescription",
+                Switch(
+                  value: settings[1],
+                  onChanged: (tog) {
+                    setState(() {
+                      settings[1] = tog;
+                    });
+                  },
+                )),
+            settingsItemBuilder(
+                "settingTitle",
+                "settingDescription",
+                Switch(
+                  value: settings[2],
+                  onChanged: (tog) {
+                    setState(() {
+                      settings[2] = tog;
+                    });
+                  },
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
