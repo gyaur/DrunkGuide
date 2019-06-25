03efc899d4820a0b2b1fa949b9c55a7ae3e102ad
Gyarmathy Bálint <bulint1@gmail.com>
2019-06-25 18:40:44 +0200

    Added appbar to settings

--- drunk_guide/lib/settings_screen.dart
+++ drunk_guide/lib/settings_screen.dart
@@ -29,6 +29,10 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings"),
+        backgroundColor: Colors.amber,
+      ),
       backgroundColor: Colors.transparent,
       body: Container(
         padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
