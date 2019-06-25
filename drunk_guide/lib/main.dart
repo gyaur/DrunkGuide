156c01df21cb8de371b23818ca43d1275efdf0a6
Gyarmathy Bálint <bulint1@gmail.com>
2019-06-25 12:21:34 +0200

    Added conditional opening page

--- drunk_guide/lib/main.dart
+++ drunk_guide/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:drunk_guide/main_screen.dart';
+import 'package:drunk_guide/settings_screen.dart';
 import 'package:flutter/material.dart';
 
 import './login_screen.dart';
@@ -11,8 +12,12 @@ class DrunkGuide extends StatelessWidget {
     return MaterialApp(
       initialRoute: '/',
       routes: <String, WidgetBuilder>{
-        '/': (context) => LoginScreen(),
-        '/MainScreen': (context) => MainScreen()
+        '/': (context) =>
+            (false) //TODO: Replace with SharedPrefernces info to check if the user is logged in
+                ? LoginScreen()
+                : MainScreen(),
+        '/MainScreen': (context) => MainScreen(),
+        '/SettingsScreen': (context) => SettingsScreen(),
       },
     );
   }
