c3944cbc5742bb706626660907cc7fc31d1867f0
Gyarmathy Bálint <bulint1@gmail.com>
2019-06-28 14:31:41 +0200

    Added logic to only show login if user is not logged in

--- drunk_guide/lib/main.dart
+++ drunk_guide/lib/main.dart
@@ -1,21 +1,40 @@
 import 'package:drunk_guide/main_screen.dart';
 import 'package:drunk_guide/settings_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import './login_screen.dart';
 
 void main() => runApp(DrunkGuide());
 
-class DrunkGuide extends StatelessWidget {
+class DrunkGuide extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _DrunkGuideState();
+  }
+}
+
+class _DrunkGuideState extends State<DrunkGuide> {
+  SharedPreferences prefs;
+  Future<bool> isLoggedIn() async {
+    prefs = await SharedPreferences.getInstance();
+    return prefs.getBool("logged_in") ?? false;
+    //TODO:Make const variables for keys
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       initialRoute: '/',
       routes: <String, WidgetBuilder>{
-        '/': (context) =>
-            (false) //TODO: Replace with SharedPrefernces info to check if the user is logged in
-                ? LoginScreen()
-                : MainScreen(),
+        '/': (context) => FutureBuilder(
+            future: isLoggedIn(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData && snapshot.data) {
+                return MainScreen();
+              }
+              return LoginScreen(prefs);
+            }),
         '/MainScreen': (context) => MainScreen(),
         '/SettingsScreen': (context) => SettingsScreen(),
       },
