cf7a99d1d2ca445a8d0570d8c9a76606b3d4ac7b
Gyarmathy Bálint <bulint1@gmail.com>
2019-06-30 21:44:49 +0200

    Added streams and db access

--- drunk_guide/lib/auth_service.dart
+++ drunk_guide/lib/auth_service.dart
@@ -1,9 +1,32 @@
+import 'package:drunk_guide/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rxdart/rxdart.dart';
 
 class AuthService {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final Firestore _db = Firestore.instance;
+
+  Observable<FirebaseUser> user;
+  Observable<User> profile;
+
+  AuthService() {
+    user = Observable(_auth.onAuthStateChanged);
+
+    profile = user.switchMap((FirebaseUser u) {
+      if (u != null) {
+        return _db
+            .collection('users')
+            .document(u.uid)
+            .snapshots()
+            .map((snap) => User.fromFirestore(snap));
+      } else {
+        return Observable.just(User());
+      }
+    });
+  }
 
   Future<FirebaseUser> handleSignIn() async {
     final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
@@ -16,8 +39,28 @@ class AuthService {
     );
 
     final FirebaseUser user = await _auth.signInWithCredential(credential);
+    updateUserData(user);
+
     print("signed in " + user.displayName);
     return user;
+  }
+
+  void updateUserData(FirebaseUser user) async {
+    DocumentReference ref = _db.collection('users').document(user.uid);
+    //TODO: Check if user has friends and dont override them
+    //if user is already in the db don't update friends
+
+    return ref.setData(
+      {
+        'uid': user.uid,
+        'email': user.email,
+        'photoURL': user.photoUrl,
+        'displayName': user.displayName,
+        'lastSeen': DateTime.now(),
+        'friends': []
+      },
+      merge: true,
+    );
   }
 }
 
