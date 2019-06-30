1b05d98c2a1d58ac6d3f388c95cb6b6083313f88
Gyarmathy Bálint <bulint1@gmail.com>
2019-06-30 21:43:50 +0200

    Added User class

---
+++ drunk_guide/lib/user.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User {
+  final String uid;
+  final String email;
+  final String photoURL;
+  final String displayName;
+  final DateTime lastSeen;
+  final List<String> friends;
+
+  User(
+      {this.uid,
+      this.email,
+      this.photoURL,
+      this.displayName,
+      this.lastSeen,
+      this.friends});
+
+  factory User.fromFirestore(DocumentSnapshot doc) {
+    Map data = doc.data;
+
+    return User(
+      uid: doc.documentID,
+      email: data['email'],
+      photoURL: data['photoURL'],
+      displayName: data['displayName'],
+      lastSeen: data['lastSeen'],
+      friends: data['friends'],
+    );
+  }
+}
